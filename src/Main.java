044fa20b3bf8fafa3aac897d6c5bf61b3826ea15
Agwe Bryan <agwedinobryan@gmail.com>
2022-08-26 03:53:42 +0100

    First commit

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+
+/**
+ * @author ab
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Java 😄");
+		System.out.println("This could work");
+
+	}
+
+}
