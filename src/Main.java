0e35c970641c7e2bd1f5f43814fc47404e3cf7a6
Agwe Bryan <agwedinobryan@gmail.com>
2022-08-27 00:22:47 +0100

    data types in java

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,33 @@
-/**
- * 
- */
 
-/**
- * @author ab
- *
- */
 public class Main {
 
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello Java 😄");
-		System.out.println("This could work");
-		System.out.println("Worked like a charm 😄");
+
+		// DATA TYPES - There are 8 primitive data types and 1 reference
+
+		/*
+		boolean boolValue = true; // true or false
+		
+		byte _byte = 100;	// -128 to 127
+		
+		short _short = 23232;	// -32,768 to 32,767
+		
+		int x = 3000;	// -2 billion to 2 billion
+		
+		long _long = 2000000; // -9q to 9q
+		
+		float _float  = 3.14f;	// Fractional number up to 6 - 7 digits
+		
+		double _double = 3.14;	// Fractional number up to 15 digits (More precision than float)
+		
+		char character = 'A';	// Single character/letter/ASCII value
+		
+		String name = "Agwe Bryan"; 	// A sequence of characters
+		
+		
+		System.out.println(name);
+		*/
+
 	}
 
 }
